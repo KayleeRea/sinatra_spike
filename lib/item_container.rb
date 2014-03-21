6ea6e45c72d67768ffeb8a5913605fa2f1933166
Kaylee Edmonson <kaylee.edmonson@gmail.com>
2014-03-21 11:30:30 -0600

    Adding item_container.rb
    Signed-off-by: Gerard Cote <grcote@gmail.com>

---
+++ lib/item_container.rb
@@ -0,0 +1,9 @@
+class ItemContainer
+  def menu
+    list_of_menu_items=
+    [Item.new(1, "Chicken Tika"),
+     Item.new(2, "Cheese Tika"),
+     Item.new(3, "Brocolli Tika")]
+    list_of_menu_items
+  end
+end
