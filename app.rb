33e860c0ec9890727793b5099fb3b28324eec559
Gerard Cote <grcote@gmail.com>
2014-03-21 15:29:57 -0600

    Modifying variable name to get Heroku app to work
    Signed-off-by: Kaylee Edmonson <kaylee.edmonson@gmail.com>

--- app.rb
+++ app.rb
@@ -23,18 +23,18 @@ get '/items' do
   erb :items
 end
 
-post '/items/new' do
-  @displayed_items = ItemContainer.new.add_menu_item(params[:new_item_name])
-  erb :items
-end
-
 get '/items/:id' do
   array_of_items = ItemContainer.new.menu
 
   array_of_items.each do |item|
     if item.id == params[:id].to_i
-      @name = item.name
+      @item_name = item.name
     end
   end
   erb :item_page
+end
+
+post '/items/new' do
+  @displayed_items = ItemContainer.new.add_menu_item(params[:new_item_name])
+  erb :items
 end
