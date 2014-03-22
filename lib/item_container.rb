7a14de5cea76d50b752ac070ac9a7362400541e7
Gerard Cote <grcote@gmail.com>
2014-03-22 08:54:16 -0600

    User can edit menu item names

--- lib/item_container.rb
+++ lib/item_container.rb
@@ -13,4 +13,23 @@ class ItemContainer
     list_of_menu_items = self.menu
     list_of_menu_items.push(Item.new(4, name))
   end
+
+  def is_on_menu?(id)
+    list_of_menu_items = self.menu
+    list_of_menu_items.each do |item|
+      if item.id == id.to_i
+        true
+      end
+    end
+  end
+
+  def update_menu_item(id, updated_item_name)
+    list_of_menu_items = self.menu
+    list_of_menu_items.each do |item|
+      if item.id == id.to_i
+        item.name.replace(updated_item_name)
+      end
+    end
+    list_of_menu_items
+  end
 end
